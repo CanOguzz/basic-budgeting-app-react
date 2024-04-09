5f87dcf2e0645281e8a1d37ee32982de0735a1f6
CanOguzz <mustafacanoguz@gmail.com>
2024-04-09 18:32:09 +0300

    Add Bootstrap and update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Container from "react-bootstrap/Container";
+import { Stack, Button } from "react-bootstrap";
 
-import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Stack direcktion="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+    </Container>
   );
 }
 
