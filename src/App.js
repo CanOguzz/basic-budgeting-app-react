1b554409e68b6e993b10859d41485f34582716f3
CanOguzz <mustafacanoguz@gmail.com>
2024-04-09 18:38:58 +0300

    Update App.js layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Container from "react-bootstrap/Container";
-import { Stack, Button } from "react-bootstrap";
-
+import { Stack, Button,BudgetCard } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
       <Stack direcktion="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard></BudgetCard>
+      </div>
     </Container>
   );
 }
